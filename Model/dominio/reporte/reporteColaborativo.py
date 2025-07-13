62f123a5088eae9a1c35e4ad41f49c59fd06c6e9
jss930 <jalfarob@unsa.edu.pe>
2025-07-13 08:33:22 -0500

    ...

--- Model/dominio/reporte/reporteColaborativo.py
+++ Model/dominio/reporte/reporteColaborativo.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
+
 
 class ReporteColaborativo:
     def __init__(self):
@@ -18,33 +20,43 @@ class ReporteColaborativo:
         self.votos_negativos = None
         self.usuarios_votantes = None
         self.es_validado = None
-
+    
+    @abstractmethod
     def cambiar_estado(self, nuevo_estado, usuario_validador_id):
         pass
 
+    @abstractmethod
     def agregar_voto_validacion(self, usuario_id, voto):
         pass
 
+    @abstractmethod
     def calcular_credibilidad(self, ):
         pass
 
+    @abstractmethod
     def esta_en_zona(self, coordenadas, radio):
         pass
 
+    @abstractmethod
     def puede_ser_editado_por(self, usuario_id):
         pass
 
+    @abstractmethod
     def es_reporte_reciente(self, ):
         pass
 
+    @abstractmethod
     def obtener_coordenadas(self, ):
         pass
 
+    @abstractmethod
     def usuario_ya_voto(self, usuario_id):
         pass
 
+    @abstractmethod
     def validar_datos_internos(self, ):
         pass
 
+    @abstractmethod
     def calcular_nivel_peligro_automatico(self, ):
         pass
