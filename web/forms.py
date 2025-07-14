3252c7f5c16069abbd02921cbad91750f7be11cf
jss930 <jalfarob@unsa.edu.pe>
2025-07-13 23:52:22 -0500

    refactor: excluir 'estado_reporte' del form, esta acción es para el admin

--- web/forms.py
+++ web/forms.py
@@ -53,17 +53,12 @@ class LoginForm(AuthenticationForm):
 class ReporteColaborativoForm(forms.ModelForm):
     class Meta:
         model = ReporteColaborativo
-        exclude = ['usuario_reportador', 'fecha_creacion', 'fecha_actualizacion', 'votos_positivos', 'votos_negativos', 'usuarios_votantes', 'es_validado']
+        exclude = ['usuario_reportador', 'fecha_creacion', 'fecha_actualizacion', 'votos_positivos', 'votos_negativos', 'usuarios_votantes', 'es_validado', 'estado_reporte']
         widgets = {
             'titulo': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título del incidente'}),
             'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Describa lo sucedido'}),
             'ubicacion': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ej: Av. Venezuela, Cercado'}),
             'tipo_incidente': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ej: Robo, incendio...'}),
             'imagen_geolocalizada': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-            'estado_reporte': forms.Select(choices=[
-                ('pendiente', 'Pendiente'),
-                ('probado', 'Probado'),
-                ('rechazado', 'Rechazado')
-            ], attrs={'class': 'form-select'}),
             'nivel_peligro': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 5}),
         }
